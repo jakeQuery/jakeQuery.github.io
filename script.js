4c87e885180cbc56f958ce5f595093e9ea77aedf
jakedemian <jakedemian@gmail.com>
2014-06-28 19:52:38 -0400

    finished contact.html, added para to main page
    I finished the baseline contact.html page and I added a section to the
    main page describing how I'm looking for clients for web development.

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ $(document).ready(function()
     //trigger quote when you scroll far enough
     $(window).on('scroll', function() {
         var y_scroll_pos = window.pageYOffset;
-        var scroll_pos_test = 500;             // set to whatever you want it to be
+        var scroll_pos_test = 900;             // set to whatever you want it to be
 
         if(y_scroll_pos > scroll_pos_test) {
             $('.quote').fadeTo(1200, 1);
